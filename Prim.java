eb75e3805795e5fedeafd935415784123cb4b5e2
SuperPanda <superpanda@OSX.local>
2013-09-21 22:01:53 -0700

    Incremental changes

---
+++ Prim.java
@@ -0,0 +1,19 @@
+/**
+ * Implements Prim's algorithm as described in the CS47B Spanning Tree Assignment
+ *
+ * Written by John Wilkey for CS47B
+ */
+public class Prim {
+  
+    solve(Graph g) {
+    
+    }
+    
+    /**
+     * args to the program should be the list of vertices.
+     */
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        solve(g);
+    }
+}
