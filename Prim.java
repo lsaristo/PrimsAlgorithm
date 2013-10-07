60789200d4496bc0675ef9350847f93b202b6f12
Class_Account <cs47b-am@hive3.CS.Berkeley.EDU>
2013-10-07 10:02:34 -0700

    Small changes

--- Prim.java
+++ Prim.java
@@ -3,18 +3,45 @@
  *
  * Written by John Wilkey for CS47B
  */
+import java.util.*;
 public class Prim {
-  
-    solve(Graph g) {
-    
-    }
-    
+	/* Graph Configuration File */
+	final static String CONFIG = "graphConfig.txt";
+
+	/* Class memebers */
+	Graph graph;  
+
+	public Prim() {
+		graph = createGraph(); 
+	}
+
+	private Graph createGraph() {
+		Graph newGraph = null;
+		try {
+			Scanner inScan = new Scanner(new BufferedReader(new FileReader(CONFIG)));
+			while(inScan.hasNext())
+				newGraph.getVertex(inScan.next(), inScan.next(), inScan.next());
+		}
+		catch (IOException e) {
+			System.out.println("ERROR: Unexpected end of configuration or no configuration found");
+		}
+		return newGraph;
+	}
+
+	private void primsAlgorithm() {
+		ArrayList<Vertex> vertList = new ArrayList<Vertex>();	
+		vertList.add(graph.getVertex("A"));
+		while(vertList.size > 0) {
+			// Breadth-first alg goes here. 
+		}
+	}
+
     /**
      * args to the program should be the list of vertices.
      */
     public static void main(String[] args) {
-		ArrayList<Vertex> tree = new Arraylist();
-        Graph g = new Graph();
-        solve(g);
-    }
-}
+		Prim puzzle = new Prim();
+		puzzle.primsAlgorithm();
+    
+	} // End main in Prim
+} // End Prim Class
